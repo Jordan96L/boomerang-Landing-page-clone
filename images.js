2064e6e439988175db905be09e4b03ca24c6448c
Jordan96L <jordan-ling@hotmail.co.uk>
2023-01-21 19:05:51 +0000

    add in images and files needed

---
+++ images.js
@@ -0,0 +1,8 @@
+<div>
+  <img src="assets/boomerang" alt="" />
+  <img src="assets/boomerangLogo" alt="" />
+  <img src="assets/hero-cars-mobile" alt="" />
+  <img src="assets/logoMain" alt="" />
+  <img src="assets/tele" alt="" />
+  <img src="asset/vector" alt="" />
+</div>;
