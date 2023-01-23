c17ff601d13817acc1fa9c6b27da403235822858
Jordan96L <jordan-ling@hotmail.co.uk>
2023-01-23 15:48:21 +0000

    add in text with animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,5 @@
 import "./landing.css"
+import Button from "./button/Button"
 
 export default function Landing () {
     return (
@@ -33,6 +34,20 @@ export default function Landing () {
                         <a href="#" className="nav-tab-link">Our cars</a>
                         <a href="#" className="nav-tab-link">FAQs</a>
                         <a href="#" className="nav-tab-link">Contact us</a>
+                    </div>
+                </div>
+
+                <div className="landing-text">
+                    <h1 className="text-heading">
+                    The <span className="word-underline">Cheapest</span> Taxi<br />
+                        You've <span className="word-underline">Ever</span> Bought.
+                    </h1>
+                    <h5 className="text-description">
+                    With Boomerang, rent a clean-air-compliant hybrid taxi from
+                        us, then keep it for just £1!
+                    </h5>
+                    <div className="text-button-container">
+                       <Button>Start Now</Button> 
                     </div>
                 </div>
             </section>
