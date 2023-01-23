86eaca181bda87d1eb4fdc3fbf617286988a524f
Jordan96L <jordan-ling@hotmail.co.uk>
2023-01-23 15:04:17 +0000

    add nav bar with slide animation

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -25,6 +25,17 @@ export default function Landing () {
             alt="cars"
             className="cars-image"
             />
+
+            <section className="landing-content">
+                <div className="nav-bar">
+                    <div className="nav-tabs">
+                    <a href="#" className="nav-tab-link">Home</a>
+                        <a href="#" className="nav-tab-link">Our cars</a>
+                        <a href="#" className="nav-tab-link">FAQs</a>
+                        <a href="#" className="nav-tab-link">Contact us</a>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
